a3418c06a53c3d8f92e854bef4eb59fc0817a916
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-10-20 23:24:55 +0200

    Dynamic column and task added

--- js/script.js
+++ js/script.js
@@ -31,6 +31,43 @@ const data = [
     },
 ]
 
+/* <section class="column">
+    <h2 class="column-title">Iniziati</h2>
+    <div class="task" draggable="true">Terminare l'esercizio di base del drag e drop</div>
+</section> */
+
+// CREAZIONE DINAMICA COLONNA E TASK
+const columnsContainer = document.querySelector("#columns-container");
+
+data.forEach(column => {
+
+    const newColumn = document.createElement("section"); // Colonna
+    newColumn.classList.add("column");
+    newColumn.setAttribute("data-id", column.id);
+    const columnTitle = document.createElement("h2");
+    columnTitle.classList.add("column-title");
+    columnTitle.textContent = column.name;
+    newColumn.append(columnTitle);
+
+    column.tasks.forEach(task => {
+
+        const newTask = document.createElement("div"); // Task
+        newTask.classList.add("task");
+        newTask.setAttribute("draggable", true);
+        newTask.setAttribute("data-id", task.id);
+        newTask.textContent = task.name;
+        newColumn.append(newTask);
+    })
+    
+    columnsContainer.append(newColumn);
+})
+
+
+
+
+
+
+
 // EVENTI DRAG E DROP
 // (https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API#concepts_and_usage)
 /*
@@ -66,7 +103,7 @@ columns.forEach(column => {
 function dragStart() {
     console.log("dragStart");
 
-    /* Appena afferro l'elemento lo nascondo aggiungendo la classe css.
+    /* Appena afferro l'elemento lo nascondo aggiungendo la classe css "d-none".
     Il tutto però è talmente rapido da non permettermi di draggarlo, per
     questo motivo devo aggiungere un deelay minimo */
     setTimeout(() => this.classList.add("d-none"), 0)
@@ -84,7 +121,7 @@ function dragEnd() {
 }
 
 function dragOver(e) {
-    e.preventDefault();  // L'evento dragover di default ci blocca "dragdrop", per evitarlo quindi usiamo preventDefault
+    e.preventDefault();  // L'evento dragover, di default, ci blocca "dragdrop", per evitarlo quindi usiamo preventDefault
     console.log("dragOver");
     
 }
